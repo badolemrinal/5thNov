68351882a6ef98a9c26a2b510076e9da6928e295
MILIND R BADOLE <MILIND R BADOLE@LAPTOP-D6FGHIJR>
2023-04-07 10:06:36 +0530

    User story 5 changes is done in xyz Testcaes related to PO corrections

--- Basic1/src/casting/opretors.java
+++ Basic1/src/casting/opretors.java
@@ -6,7 +6,7 @@ public class opretors {
 		
 		
 		System.out.println( (10>3) && (12>7)); //true
-		System.out.println( (10>5) && (12>8)); //unknown  
+		System.out.println( (10>4) && (12>8)); //unknown  
         System.out.println( (10>3) && (12<7));  //false
 		
 	// or operator	
